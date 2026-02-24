82821c88d332c0b16feaa07886b128ae9ec23df2
Mirindra Lucien LANTOSOA <MRD@setex1.lan>
2026-02-24 11:56:07 +0100

    update parameter

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const http = require("http")
 const {spawn} = require("child_process")
 
+require("dotenv").config()
+const port = process.env.PORT || 5500
 const cmd = 'docker'
-const args = ["logs", "-f", "avr-core-google-7"]
+const args = ["logs", "-f", "avr-core-google-10"]
 const patterns = [
     "Received data from external asr service",
     "Sends text from LLM to TTS"
 ] 
-const serveur = http.createServer();
-
-serveur.on("request", (req, res) => {
+const serveur = http.createServer((req, res) => {
     const pythonVersion = spawn(cmd, args);
     pythonVersion.stdout.on("data", (data) => {
         res.write(data.toString())
@@ -18,8 +18,8 @@ serveur.on("request", (req, res) => {
     pythonVersion.stderr.on("data", (data) => {
         res.end(data.toString())
     })
-})
+});
 
-serveur.listen(5500, () => {
-    console.log("server run on : http://localhost:5500")
+serveur.listen(port, () => {
+    console.log(`server run on : http://localhost:${port}`)
 })
