517583e128a6679b13f055b873e1e9922e2313ab
Mirindra Lucien LANTOSOA <MRD@setex1.lan>
2026-03-12 06:45:52 +0100

    testing without interface

---
+++ main.js
@@ -0,0 +1,18 @@
+const { spawn } = require("child_process")
+
+dockerLogs = spawn("docker", ["logs", "-f", "--since=1s", "avr-core-google-10"])
+
+dockerLogs.stdout.on("data", (data) => {
+    console.log("data");
+});
+dockerLogs.stdout.on("end", () => {
+    console.log("Terminate task");
+});
+
+dockerLogs.stderr.on("data", (data) => {
+    console.log(data);
+});
+
+dockerLogs.stderr.on("end", () => {
+    console.log("Error terminate");
+});
